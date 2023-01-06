72cee4b7def7d4d41e534cbc427acb461b209117
apran42 <flyingdisc1@naver.com>
2023-01-07 05:32:44 +0900

    boolean 자료형
    Signed-off-by: apran42 <flyingdisc1@naver.com>

---
+++ practice3.py
@@ -0,0 +1,18 @@
+# 참 / 거짓
+
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+# 결과
+# False
+# True
+# True
+# False
+# False
+# True
+# True
