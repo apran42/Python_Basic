5da717fbddbb3089f94941507143915c3c7ed6a8
apran42 <flyingdisc1@naver.com>
2023-01-08 04:34:54 +0900

    탈출 문자
    Signed-off-by: apran42 <flyingdisc1@naver.com>

---
+++ chapter3/practice_3_5.py
@@ -0,0 +1,18 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" or \' : 문장 내에서 따옴표
+print("저는 \"아프란\"입니다")
+print("저는 \'아프란\'입니다")
+
+# \\ : 문장 내에서 \
+print("C:\\Users\\아프란\\Desktop\\python\\chapter3\\")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
