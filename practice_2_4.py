02a7ebba3d4f71b697d20af5381f0ac28552854f
apran42 <flyingdisc1@naver.com>
2023-01-08 02:38:01 +0900

    변수 및 주석
    Signed-off-by: apran42 <flyingdisc1@naver.com>

---
+++ practice_2_4.py
@@ -0,0 +1,19 @@
+# 애완동물을 소개해 주세요
+animal = "고양이"
+name = "해피"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+'''
+이렇게 하면
+여러 문장을
+주석 처리할
+수 있습니다
+'''
+
+print("우리집 " + animal +  "의 이름은 " + name + "에요")
+hobby = "낮잠"
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
