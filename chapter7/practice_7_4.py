4c7fa44e7eec6739ad07f27331ba5b6224f241de
apran42 <flyingdisc1@naver.com>
2023-01-10 04:55:02 +0900

    pickle
    Signed-off-by: apran42 <flyingdisc1@naver.com>

---
+++ chapter7/practice_7_4.py
@@ -0,0 +1,14 @@
+import pickle
+
+# 입력
+# profile_file = open("profile.pickle", "wb")
+# profile = {"이름": "박명수", "나이": 30, "취미": ["축구", "골프", "코딩"]}
+# print(profile)
+#
+# pickle.dump(profile, profile_file)  # profile에 있는 정보를 profile_file에 저장
+# profile_file.close()
+
+profile_file = open("profile.pickle", "rb")
+profile = pickle.load(profile_file)  # profile_file에 있는 정보를 profile 에 불러오기
+print(profile)
+profile_file.close()
