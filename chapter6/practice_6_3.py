7f204109f6409bda1d166ec58d310941a638a5e9
apran42 <flyingdisc1@naver.com>
2023-01-10 03:40:07 +0900

    기본값
    Signed-off-by: apran42 <flyingdisc1@naver.com>

---
+++ chapter6/practice_6_3.py
@@ -0,0 +1,12 @@
+# def profile(name, age, main_lang):
+#     print(f"이름 : {name}\t나이 : {age}\t주 사용 언어 : {main_lang}")
+#
+# profile("유재석", 20, "파이썬")
+# profile("킴태호", 25, "자바")
+
+def profile(name, age=17, main_lang="파이썬"):
+    print(f"이름 : {name}\t나이 : {age}\t주 사용 언어 : {main_lang}")
+
+
+profile("유재석")
+profile("김태호")
