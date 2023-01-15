d140569f8325dd4114ca7aef8dcb18a08193d738
apran42 <flyingdisc1@naver.com>
2023-01-16 04:57:32 +0900

    퀴즈 #10 (모듈)
    Signed-off-by: apran42 <flyingdisc1@naver.com>

--- chapter10/byme.py
+++ chapter10/byme.py
@@ -1,4 +1,4 @@
 def sign():
-    print("이 프로그램은 아프란(닉네임)에 의해 만들어졌습니다.")
+    print("이 프로그램은 아프란(apran42)에 의해 만들어졌습니다.")
     print("깃허브 : https://github.com/apran42")
     print("이메일 : flyingdisc1@naver.com")
